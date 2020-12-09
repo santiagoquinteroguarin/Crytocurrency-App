dae2a822e55ad7496cad14e20eb9721225c638a0
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-09 11:58:14 -0500

    Creating styled component for summary

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResponseDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Price = styled.p`
+    font-size: 30px;
+
+    span {
+        font-weight: bold;
+    }
+`;
 
 const Calculate = ({response}) => {
 
     if(Object.keys(response).length === 0) return null;
 
     return (
-        <div>
-            <p>El precio es: <span>{response.PRICE}</span></p>
-            <p>El precio más alto del día es: <span>{response.HIGHDAY}</span></p>
-            <p>El precio más bajo del día es: <span>{response.LOWDAY}</span></p>
-            <p>Variación últimas 24 horas: <span>{response.CHANGEPCT24HOUR}</span></p>
-            <p>Última Actualización: <span>{response.LASTUPDATE}</span></p>
-        </div>
+        <ResponseDiv>
+            <Price>El precio es: <span>{response.PRICE}</span></Price>
+            <Info>El precio más alto del día es: <span>{response.HIGHDAY}</span></Info>
+            <Info>El precio más bajo del día es: <span>{response.LOWDAY}</span></Info>
+            <Info>Variación últimas 24 horas: <span>{response.CHANGEPCT24HOUR}</span></Info>
+            <Info>Última Actualización: <span>{response.LASTUPDATE}</span></Info>
+        </ResponseDiv>
     );
 }
 
