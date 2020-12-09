9cf8e7b37fa858a5f638c7341b97fc58c709ddb3
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-09 10:48:12 -0500

    Creating styled components for the Label and the SelectTag

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -1,4 +1,26 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFFFFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const SelectTag = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    --webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    font-size: 1.2rem;
+`;
 
 const useCoin = (label, initialState, options) => {
 
@@ -8,13 +30,16 @@ const useCoin = (label, initialState, options) => {
     // crear en PascalCase porque va ser un componente
     const Select = () => (
         <Fragment>
-            <label>{label}</label>
-            <select>
+            <Label>{label}</Label>
+            <SelectTag
+                onChange={e => setUpdateState(e.target.value)}
+                value={state}
+            >
                 <option value="">-- Seleccione --</option>
                 {options.map(options => (
                     <option key={options.code} value={options.code}>{options.name}</option>
                 ))}
-            </select>
+            </SelectTag>
         </Fragment>
     )
 
